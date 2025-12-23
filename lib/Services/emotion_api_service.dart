40136d0eba50bd9cdc2e1c8b5e9d3969d85df7ae
Darshan Varu <darshanvaru2003@gmail.com>
2025-12-23 20:28:20 +0530

    Refactor build.gradle and EmotionApiService for improved clarity and consistency

--- lib/Services/emotion_api_service.dart
+++ lib/Services/emotion_api_service.dart
@@ -2,15 +2,12 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:camera/camera.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
 
 class EmotionApiService {
   static Future<String> getEmotion(XFile image) async {
 
-    // final prefs = await SharedPreferences.getInstance();
     final ip = "https://darshanvaru-emotion-eye-detector.hf.space/predict";
 
-    debugPrint("_______________Pref Link in emotion_api_service.dart: $ip");
     final request = http.MultipartRequest(
       'POST',
       Uri.parse(ip),
@@ -19,12 +16,12 @@ class EmotionApiService {
 
     final response = await request.send();
     final resBody = await response.stream.bytesToString();
-    debugPrint("_______________ Status: ${response.statusCode}");
+    debugPrint("[EmotionApiService] Status of Request: ${response.statusCode}");
 
     if (response.statusCode == 200) {
       final decoded = jsonDecode(resBody);
       final emotion = decoded['emotion'] ?? 'unknown';
-      debugPrint("_______________ Label: $emotion");
+      debugPrint("[EmotionApiService] Received Label from Server: $emotion");
 
       return emotion;
     } else {
